26c7d003e62411abf8e0cb73093797992bf01cf1
yang <billcome2000@gmail.com>
2021-07-07 20:48:44 +0800

    Initial Commit

---
+++ elss1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  elss1
+//
+//  Created by 洋學銘 on 2021/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
